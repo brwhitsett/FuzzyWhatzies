43bb32556246863a881898ddbaeb75c4de4e13ee
Dusan <dmomcilo@hotmail.com>
2022-04-12 11:03:09 -0400

    backend finished I think

--- functions/src/models/User.ts
+++ functions/src/models/User.ts
@@ -3,7 +3,7 @@ import { ObjectId } from "mongodb";
 export default interface User {
   _id?: ObjectId;
   uid: string;
-  name: string;
+  displayName: string;
   eC: number;
   eI: number;
   eT: number;
